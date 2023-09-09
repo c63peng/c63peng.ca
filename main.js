bf5b234ba1534e1e8af3b36c25eb983d2a6766fb
Cindy Peng <cindypeng80@gmail.com>
2023-09-09 17:39:19 -0400

    images updated

---
+++ main.js
@@ -0,0 +1,30 @@
+function paralloxScrolling(){
+    let stars = document.getElementById('stars');
+    let moon = document.getElementById('moon');
+    let mountains_behind = document.getElementById('mountains_behind');
+    let btn = document.getElementById('btn');
+    let mountains_front = document.getElementById('mountains_front');
+    let header = document.querySelector('header')
+
+    window.addEventListener('scroll',function(){
+        let value = window.scrollY;
+        stars.style.left = value * 0.25 + 'px';
+        moon.style.top = value * 1.05 + 'px';
+        mountains_behind.style.top = value * 0.5 + 'px';
+        mountains_front.style.top = value * 0 + 'px';
+        text.style.marginRight = value * 4 + 'px';
+        text.style.marginTop = value * 1.5 + 'px';
+        btn.style.marginTop = value * 1.5 + 'px';
+        header.style.top = value * 0.5 + 'px';
+    })
+}
+
+
+function scrollingStars() {
+    let shootingstar = document.getElementById('shooting-star');
+            
+    window.addEventListener('scroll',function(){
+    let value = window.scrollY;
+    shootingstar.style.left = value * 0.40 + 'px';});
+}
+
